f7adf1b0432aa0ee38a7e88062ef03acad17d314
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-04-27 22:32:52 -0300

    Update Cadastro.php

--- app/models/Cadastro.php
+++ app/models/Cadastro.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Cadastro extends Model
 {
     protected $table = 'cadastros';
+
+    protected $fillable = 
+    [
+        'nome', 
+        'email', 
+        'idade', 
+        'senha',         
+    ];  
+
+    //função de relacionamento com receitas sendo (1 cadastro para muitas receitas)
+    public function receitas()
+    {
+        return $this->hasMany('App\models\Receitas', 'cadastro_id')
+    }
 }
