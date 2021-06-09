41b92e5629eabe58e2fe697fe20a75acddb70f0b
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-06-09 15:30:25 -0300

    Create CadastroRestController.php

---
+++ app/Http/Controllers/Rest/CadastroRestController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cadastro;
+use Illuminate\Http\Request;
+use App\Http\Requests\CadastroRequest;
+
+class CadastroController extends Controller
+{
+    private $repository;
+    protected $request;
+
+    public function __construct(Request $request, Cadastro $cadastro)
+    {
+        $this->repository = $cadastro;
+        $this->request = $request;
+    }
+
+    //retorna a pagina de listagem de usuarios    
+    public function index(Request $request)
+    {    
+        $registros = $this->repository->paginate(15);
+        return response()->json($registros);
+    }
+
+    public function search(Request $request)
+    {
+        $filters = $request->all();
+        $registros = $this->repository->search($request->nome);
+
+        return view('cadastro.index', [
+            'registros' => $registros,
+            'filters' => $filters,
+        ]);
+    }
+
+    //retorna a pagina para cadastrar um novo usuario
+    public function new()
+    {
+        return view('cadastro.incluir');
+    }
+
+    //salvar o registro de um novo usuario
+    public function create(Request $request)
+    {
+        $registro = $request->all();
+        $this->repository->create($registro);  
+        return response()->json($registros);      
+    }
+
+    //retorna o registro de um usuario para alteração dos dados
+    public function update( $id)
+    {
+        $registro = $this->repository->find($id);
+
+        if(!$registro)
+        {
+            return redirect()->back();
+        }
+
+        return view('cadastro.alterar', [
+            'registro' => $registro,
+        ]);
+    }
+
+    //retorna o registro de um usuario para exclusão
+    public function delete( $id)
+    {
+        $registro = $this->repository->find($id);
+
+        if(!$registro)
+        {
+            return redirect()->back();
+        }
+
+        return view('cadastro.excluir', [
+            'registro' => $registro,
+        ]);
+    }
+
+    //retorna o registro para consulta/vizualização na tela 
+    public function view($id)
+    {
+        $registro = $this->repository->find($id);
+
+        if(!$registro)
+        {
+            return redirect()->back();
+        }
+
+        return view('cadastro.consultar', [
+            'registro' => $registro,
+        ]);
+    }
+
+    //alterar o registro modificado
+    public function save(CadastroRequest $request, $id)
+    {
+        $data = $request->all();
+
+        $registro = $this->repository->find($id);
+        
+        $registro->update($data);
+        return redirect()->route('cadastro.listar');
+    }
+
+    //excluir o registro do usuario
+    public function excluir(Request $request, $id)
+    {
+        $registro = $this->repository->find($id);
+        $registro->delete();
+        return redirect()->route('cadastro.listar');
+    }
+
+    //cancela a operação do usario
+    public function cancel()
+    {
+        return redirect()->route('cadastro.listar');
+    }
+}
